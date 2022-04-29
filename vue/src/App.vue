9684932ccfe9809523587cc6c9f773b98046baf9
Z-zia <ziasan@hotmail.co.jp>
2022-04-29 23:49:29 +0900

    Add pattern of vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn">Hello daisyui</button>
+	<button class="btn btn-primary">One</button>
+	<button class="btn btn-secondary">Two</button>
+	<button class="btn btn-accent btn-outline">Three</button>
+	<div class="dropdown dropdown-right">
+  <label tabindex="0" class="btn m-1">Click</label>
+  <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
+</template>
